bef5d43264102e097b07ca221a350f7c438edf43
vachito <valen-charlone@hotmail.com>
2022-11-24 19:35:06 -0300

    modificando css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-    return <footer className="mt-8 text-center dark:text-gray-300">Drag abd drop...</footer>;
+    return <footer className="mt-12 text-center dark:text-gray-300">Drag abd drop...</footer>;
 }
 
 export default Footer;
